d4fc99d33c62c5da8a644f901d2ae2b758f16661
Clément <clmntrss.pro@gmail.com>
2018-03-16 16:57:10 +0100

    css and form

--- resources/views/upload.blade.php
+++ resources/views/upload.blade.php
@@ -1,6 +1,6 @@
 @extends("layouts.app")
 @section('content')
-    ​
+    ​<link href="{{ asset('css/home.css') }}" rel="stylesheet">
     <form method="POST" action="/create-song" enctype="multipart/form-data">
         {{ csrf_field() }}
         ​
@@ -8,10 +8,10 @@
         <br/>
         <label>Style</label><input type="text" name="style" placeholder="Style de la chanson">
         <br/>
-        <input type="file" name="chanson">
+        <input type="file" style="display: none" class="choose-file" name="chanson"><label for="chanson">Issou</label>
         <br/>
         {{csrf_field()}}
-        <input type="submit" value="Poster">
+        <input type="submit" class="upload-file" value="Poster">
     </form>
     ​
 @endsection

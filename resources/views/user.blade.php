bfeab233ef5a905ea2959da4336d7ceb7b14ecb0
Léo Nicaisse <leonicaisse@gmail.com>
2018-03-20 19:01:45 +0100

    des bails de ouf

--- resources/views/user.blade.php
+++ resources/views/user.blade.php
@@ -6,6 +6,17 @@
     <div id="main-content-home">
         <div class="welcome-home">
             <h1 class="main-title-home"><span>{{$utilisateur->name }}</span></h1>
+            Abonnés : {{$utilisateur->ilsMeSuivent()->count()}}| Abonnements : {{$utilisateur->jeLesSuis()->count()}}
+            <br/>
+            @auth
+                @if($utilisateur->id != \Illuminate\Support\Facades\Auth::id())
+                    @if(Auth::user()->jeLesSuis->contains($utilisateur->id))
+                        <a href="/suivre/{{$utilisateur->id}}">Suivi</a>
+                    @else
+                        <a href="/suivre/{{$utilisateur->id}}">Suivre</a>
+                    @endif
+                @endif
+            @endauth
         </div>
         <div id="content-home">
             @include('_chansons', ['chansons' => $utilisateur -> chansons]);

fca57532df94969ec525d787a77d32e891f9d132
Léo Nicaisse <leonicaisse@gmail.com>
2018-03-20 19:08:21 +0100

    des bails de ouf

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -19,16 +19,12 @@
     <div class="content">
         <div class="main-landing">
             <div class="text-landing">
-                @guest
                     <h1 class="title-landing">Votre <span>musique</span>,<br/>à portée de main</h1>
                     <h2 class="subtitle-landing">Accèdez à des millions de titres en écoute illimitée</h2>
                     <ul>
                         <li><img src="images/check_orange.svg">Créez vos propres playlists</li>
                         <li><img src="images/check_orange.svg">Importez et partagez vos morceaux</li>
                     </ul>
-                @else
-                    <h1>WIP</h1>
-                @endguest
             </div>
         </div>
     </div>

01ce5121da03d49158c7285c83c70fa589de49c3
Clément <clmntrss.pro@gmail.com>
2018-03-20 15:53:40 +0100

    Upload css

--- resources/views/upload.blade.php
+++ resources/views/upload.blade.php
@@ -1,19 +1,45 @@
 @extends("layouts.app")
 @section('content')
-    ​<link href="{{ asset('css/home.css') }}" rel="stylesheet">
-    <div class="main-connexion">
-        <h1>Mettre en ligne une musique</h1>
-    <form method="POST" action="/create-song" enctype="multipart/form-data">
-        {{ csrf_field() }}
-        ​
-        <label>Nom</label><fieldset><input type="text" name="nom" id="song-name" placeholder="Nom de la chanson"></fieldset>
-        <br/>
-        <label>Style</label><fieldset><input type="text" name="style" id="style" placeholder="Style de la chanson"></fieldset>
-        <br/>
-        <input type="file" style="display: none" class="choose-file" name="chanson"><label for="chanson">Issou</label>
-        <br/>
-        {{csrf_field()}}
-        <input type="submit" class="upload-file" value="Poster">
-    </form>
-        ​</div>
+    <link href="{{ asset('css/home.css') }}" rel="stylesheet">
+    <body>
+    <div id="main-content-home">
+        <div class="welcome-home">
+            <h1 class="main-title-home">Uploader sur Soun<span>dB</span>ase
+            </h1>
+        </div>
+        <div id="content-home">
+
+            <form id="upload" method="POST">
+                <div class="grey-background">
+                    {{ csrf_field() }}
+                    <div class="form-upload-left">
+                        <label id="choose-image-label" for="choose-image">Mettre à jour</label>
+                        <input type="file" name="choose-image" id="choose-image" required>
+                    </div>
+                    <div class="form-upload-right">
+                    <fieldset>
+                        <h4 class="blue-title">Titre<span>*</span></h4>
+                        <input placeholder="Titre de la chanson" type="text" name="song-name" id="song-name" tabindex="1" required>
+                    </fieldset>
+                    <fieldset>
+                        <h4 class="blue-title">Genre<span>*</span></h4>
+                        <input placeholder="Genre de la chanson" type="text" name="style" id="style" tabindex="2" required>
+                    </fieldset>
+                    <fieldset>
+                        <h4 class="blue-title" >Fichier<span>*</span></h4>
+                        <label id="song-file-label" for="choose-file">Ajouter un fichier</label>
+                        <input placeholder="Ajouter un fichier" type="file" name="choose-file" id="choose-file" tabindex="3" required >
+                    </fieldset>
+                    <p class="blue-title italic-paragraph">En cliquant sur le bouton uploader je reconnais avoir pris connaissances des <span><a href="#" class="link-orange">Conditions Générales d'Utilisation de SoundBase</a></span> et je certifie être propriétaires des droits relatifs au fichier mis en ligne</p>
+                    </div>
+                </div>
+                <div class="button-submit">
+                    <button type="submit" class="upload-file" id="upload-file" name="upload-file">Uploader</button>
+                </div>
+
+            </form>
+
+        </div>
+    </div>
+    </body>
 @endsection
